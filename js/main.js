47942c887975b54510e5b6b63b762e18378d442c
noah <moonlight205@naver.com>
2023-09-20 16:38:08 +0900

    리뉴얼

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   //알람
   $('.alert').click(function () {
-    $('.alert_popup').addClass('active');
+    $('.alert_popup').toggleClass('active');
   });
   $('.alert_popup_close').click(function () {
     $('.alert_popup').removeClass('active');
+    $('body').removeClass('hidden');
   });
 
   //언어 버튼
@@ -24,21 +25,11 @@ $(document).ready(function () {
     adaptiveHeight: true,
   });
 
-  //search 마우스오버
-  $('.input_wrap').mouseover(function () {
-    $(this).addClass('active');
-    $(this).siblings('.title').addClass('active');
-  });
-
-  $('.input_wrap').mouseout(function () {
-    $(this).removeClass('active');
-    $(this).siblings('.title').removeClass('active');
-  });
-
   //faq 탭
   $('ul.tab').on('click', 'li', function () {
-    var tab_id = $(this).attr('data-tab');
+    let tab_id = $(this).attr('data-tab');
     $('.qna_list').removeClass('active');
+    $('.que').removeClass('active');
     if ($('ul.tab').hasClass('mytab') == false) {
       $('.qna_list .ans').removeClass('active');
       $('.qna_list:first-child .que').addClass('active');
@@ -52,12 +43,17 @@ $(document).ready(function () {
     $('.' + tab_id).addClass('active');
   });
 
+  //lang 버튼
+  $('.lang_btn.on').click(function () {
+    $(this).siblings('.lang_btn.off').toggleClass('active');
+  });
+
   //faq 내부
   $('.qna_list .que').click(function () {
     $(this).parent('.qna_list').siblings().children('.que').removeClass('active');
-    $(this).addClass('active');
+    $(this).toggleClass('active');
     $(this).parent('.qna_list').siblings().children('.ans').removeClass('active');
-    $(this).siblings('.ans').addClass('active');
+    $(this).siblings('.ans').toggleClass('active');
   });
 
   //이용약관 팝업
@@ -85,19 +81,19 @@ $(document).ready(function () {
   });
 
   //외부영역 클릭 시 팝업 닫기
-  $(document).mouseup(function (e) {
-    const popups = ['.popup_Privacy', '.popup_term'];
+  // $(document).mouseup(function (e) {
+  //   const popups = ['.popup_Privacy', '.popup_term'];
 
-    popups.forEach((popupSelector) => {
-      const layerPopup = $(popupSelector);
-      if (layerPopup.has(e.target).length === 0) {
-        layerPopup.hide();
-      }
-    });
+  //   popups.forEach((popupSelector) => {
+  //     const layerPopup = $(popupSelector);
+  //     if (layerPopup.has(e.target).length === 0) {
+  //       layerPopup.hide();
+  //     }
+  //   });
 
-    $('.popup_bg').hide();
-    $('body').removeClass('hidden');
-  });
+  //   $('.popup_bg').hide();
+  //   $('body').removeClass('hidden');
+  // });
 
   //로그인 버튼
   $('.login button').click(function () {
@@ -108,12 +104,15 @@ $(document).ready(function () {
   //헤더 모바일메뉴
   $('.mobile_menu_btn').click(function () {
     $('.head_contents').addClass('active');
+    $('body').addClass('hidden');
   });
   $('.close_mobile_btn').click(function () {
     $('.head_contents').removeClass('active');
+    $('body').removeClass('hidden');
   });
   //모바일 알람
   $('.mobile_alert').click(function () {
     $('.alert_popup').addClass('active');
+    $('body').addClass('hidden');
   });
 });
